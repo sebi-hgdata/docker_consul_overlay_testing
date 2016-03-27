54eaddf5c2222aefa9c0f811741fe3b21bef3828
Sebi Calbaza <sebastian.calbaza@softvision.ro>
2016-03-27 21:19:11 +0300

    readd setup commands

--- scripts/setup_host.sh
+++ scripts/setup_host.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+sudo apt-get update
+set -e
+sudo apt-get install -qy software-properties-common
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update	
+sudo apt-get install -y vagrant ansible python-setuptools
+	
+(cd ../vagrant && vagrant up)
 
+./check_ssh.sh
+./update_kernel.sh
+./check_ssh.sh
+./setup_consul.sh
 ./setup_docker.sh
 ./deploy_pings.sh
