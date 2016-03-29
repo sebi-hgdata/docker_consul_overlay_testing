bdceec7e6f7000a0725ff864b98b8475676d86d9
Sebi Calbaza <sebastian.calbaza@softvision.ro>
2016-03-30 00:57:05 +0300

    adding vagrant controll machine

--- ansible/inventories/custom/hosts.py
+++ ansible/inventories/custom/hosts.py
@@ -5,28 +5,28 @@ def get_hosts():
 	hosts= [
 		{
                      'ansible_host': '192.168.50.2',
- 		     'ansible_ssh_private_key_file':'~/.vagrant.d/insecure_private_key',
+ 		     'ansible_ssh_private_key_file':'files/keys/test',
                      'lables': ['s1']
                 },
                 {
                      'ansible_host': '192.168.50.3',
- 		     'ansible_ssh_private_key_file':'~/.vagrant.d/insecure_private_key',
+ 		     'ansible_ssh_private_key_file':'files/keys/test',
                      'lables': ['s2']
                 }
 ,
                 {
                      'ansible_host': '192.168.50.4',
- 		     'ansible_ssh_private_key_file':'~/.vagrant.d/insecure_private_key',
+ 		     'ansible_ssh_private_key_file':'files/keys/test',
                      'lables': ['s3']
                 },
                 {
                      'ansible_host': '192.168.50.5',
- 		     'ansible_ssh_private_key_file':'~/.vagrant.d/insecure_private_key',
+ 		     'ansible_ssh_private_key_file':'files/keys/test',
                      'lables': ['a1']
                 },
                 {
                      'ansible_host': '192.168.50.6',
- 		     'ansible_ssh_private_key_file':'~/.vagrant.d/insecure_private_key',
+ 		     'ansible_ssh_private_key_file':'files/keys/test',
                      'lables': ['a2']
                 }
         ]
